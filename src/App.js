7b7e3e5e93f07939caadd5e18e1475c14e0a6ec9
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2021-03-23 09:49:47 +0530

    Anothr tray to deploy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-          Learn React
+          Learn Reactt
         </a>
       </header>
     </div>
